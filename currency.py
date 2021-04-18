9d2f1b58f7e04a132107c6e3d05df5495a1a2514
jack456054 <jack456054@hotmail.com>
2021-04-18 23:30:57 +0800

    feat(/currency/): add hsbc currency and directly buy NTD suggection

--- currency.py
+++ currency.py
@@ -28,7 +28,7 @@ def send_notify(token, msg, filepath=None, stickerPackageId=None, stickerId=None
     return r.status_code, r.text
 
 
-def get_currency(currency_attr: str) -> str:
+def get_currency_bot(currency_attr: str) -> str:
     url = 'https://rate.bot.com.tw/xrt?Lang=zh-TW'
     res = pd.read_html(url)
     df = res[0]
@@ -55,7 +55,54 @@ def get_currency(currency_attr: str) -> str:
     return currency
 
 
+def get_currency_hsbc(currency_attr: str) -> str:
+    url = 'https://www.apps1.asiapacific.hsbc.com/1/2/Misc/popup-tw/currency-calculator'
+    res = pd.read_html(url)
+    df = res[1]
+    currency = df.iloc[3:, :5]
+    currency.columns = [u'幣別', u'即期匯率-本行買入', u'即期匯率-本行賣出', u'現金匯率-本行買入', u'現金匯率-本行賣出']
+    currency = currency.set_index(u'幣別')
+    currency = currency.filter(like=currency_attr, axis=0)
+    sell_price = currency.iloc[0][u'即期匯率-本行賣出']
+    currency = currency.T.to_csv()
+    currency = list(csv.reader(StringIO(currency)))
+    del currency[0]
+    currency = tabulate(currency, tablefmt='plain')
+    time = (datetime.now(timezone.utc) + timedelta(hours=8)).strftime("資料抓取時間：%m/%d/%Y, %H:%M:%S")
+    source = '資料來源：HSBC'
+    currency_type = f'幣別：{currency_attr}'
+    currency = f'''
+
+{currency_type}
+{currency}
+
+{time}
+{source}
+'''
+    return currency, sell_price
+
+
+def check_direct_buy(sell_price_usd: float, sell_price_cny: float) -> str:
+    c_n_ratio = sell_price_cny / sell_price_usd
+    return f'''
+即期匯率-本行賣出(CYN)/ 即期匯率-本行賣出(USD) = {c_n_ratio}
+
+* USD 買價 > {1 / c_n_ratio}(CNY)
+應直接換匯 CNY -> NTD
+
+* USD 買價 < {1 / c_n_ratio}(CNY)
+應換匯兩次 CNY -> USD -> NTD
+'''
+
+
 if __name__ == '__main__':
     token = os.environ.get('TOKEN')
-    send_notify(token=token, msg=get_currency('USD'))
-    send_notify(token=token, msg=get_currency('CNY'))
+    # token = TOKEN
+    # send_notify(token=token, msg=get_currency_bot('USD'))
+    # send_notify(token=token, msg=get_currency_bot('CNY'))
+    currency, sell_price_usd = get_currency_hsbc('USD')
+    send_notify(token=token, msg=currency)
+    currency, sell_price_cny = get_currency_hsbc('CNY')
+    send_notify(token=token, msg=currency)
+    msg = check_direct_buy(float(sell_price_usd), float(sell_price_cny))
+    send_notify(token=token, msg=msg)
